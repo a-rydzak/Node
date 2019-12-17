2769830e465221f75dae10a5f9245ba3db3922fa
Andrew Rydzak <andrew.rydzak@aa.com>
2019-12-17 11:25:04 -0600

    added webpage log

--- Callback Example/example-2.js
+++ Callback Example/example-2.js
@@ -2,16 +2,16 @@ const fs = require('fs');
 var content;
 // First I want to read the file
 function processFile() {
-    console.log(content);
+  console.log(content);
+  console.log('Loading the Webpage now');
 }
 
 fs.readFile('input.txt', function read(err, data) {
-    if (err) {
-        throw err;
-    }
-    content = data;
-    // Invoke the next step here however you like
-    console.log(content);   // Put all of the code here (not the best solution)
-    processFile();          // Or put the next step in a function and invoke it
+  if (err) {
+    throw err;
+  }
+  content = data;
+  // Invoke the next step here however you like
+  console.log(content); // Put all of the code here (not the best solution)
+  processFile(); // Or put the next step in a function and invoke it
 });
-
